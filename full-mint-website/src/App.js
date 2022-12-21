9c520e9a5aa40349d6aa459a9138ca56666dc630
Cesar Gonzalez <gcesar13@yahoo.com>
2022-12-21 07:09:57 -0700

    app.js update plus assets downlaod

--- full-mint-website/src/App.js
+++ full-mint-website/src/App.js
@@ -1,10 +1,15 @@
-
+import { useState } from 'react';
 import './App.css';
+import MainMint from './MainMint';
+import NavBar from './NavBar';
 
 function App() {
+  const [accounts, setAccounts] = UseState([]);
+
   return (
     <div className="App">
-     
+     <NavBar accounts={accounts} setAccounts={setAccounts} />
+     <MainMint accounts={accounts} setAccounts={setAccounts} />
     </div>
   );
 }
