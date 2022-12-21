f90ac84ebb1e412270400954224e4c272812a475
Cesar Gonzalez <gcesar13@yahoo.com>
2022-12-21 07:25:49 -0700

    navbar

---
+++ full-mint-website/src/NavBar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const NavBar =({ accounts, setAccounts}) => {
+    const isConnected = Boolean(accounts[0]);
+
+    async function connectAccount() {
+     if(window.ethereum) {
+        const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts"
+    });
+    setAccounts(accounts);
+}
+}
+
+return (
+    <div>
+        <div>FaceBook</div>
+        <div>Twitter</div>
+        <div>Email</div>
+
+        <div>About</div>
+        <div>Mint</div>
+        <div>Team</div>
+
+        {isConnected ? (
+        <p>Connected</p>
+        ) : (
+            <button onCLick={connectAccount}>Connect</button>
+        )}
+
+    </div>
+)
+};
+
+export default NavBar;
